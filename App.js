85e596c44dab2b666e61174bfbfe6805d217f7a5
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-03-31 14:42:08 +0530

    platform specific

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import {Button, SafeAreaView, StyleSheet} from 'react-native';
+import {Button, Platform, SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={[styles.container, containerStyles]}>
+    <SafeAreaView style={styles.container}>
       <Button title={'Click me'} onPress={() => console.log('clicked')}/>
     </SafeAreaView>
   );
@@ -12,11 +12,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
 });
-
-const containerStyles = {
-  backgroundColor: 'orange'
-}
