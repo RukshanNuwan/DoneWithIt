303649525a40c9f32706007fd609956b47b588a1
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-03-31 12:43:06 +0530

    image

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  const handlePress = () => {
-    console.log('text pressed');
-  };
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={2} onPress={handlePress}>
-        Hello react native - a really really long text. now I wanna make this even longer and see what happens!
-      </Text>
+      <Text>Hello react native</Text>
+      <Image
+        fadeDuration={300}
+        blurRadius={10}
+        source={
+          {
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300'
+          }
+        }
+      />
     </SafeAreaView>
   );
 }
