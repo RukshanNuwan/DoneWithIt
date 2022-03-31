f2945f942a477500099cab69102a4e67befd2ae5
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-03-31 14:17:58 +0530

    alert

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import {Button, SafeAreaView, StyleSheet} from 'react-native';
+import {Alert, Button, SafeAreaView, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Button
-        title={'click me'}
-        onPress={() => alert('button clicked')}
+        title={'Click me'}
+        onPress={() => Alert.alert('My title', 'My message', [
+          {text: 'Yes', onPress: () => console.log('pressed yes')},
+          {text: 'No', onPress: () => console.log('pressed no')}
+        ])}
         color={'orange'}
+      />
+
+      <Button
+        title={'Prompt button'}
+        // this api works with only ios
+        onPress={() => Alert.prompt('My title', 'My message', (text) => console.log(text))}
       />
     </SafeAreaView>
   );
