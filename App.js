bb98d3ce50c26b8d2d0951e6cd58e95bd4838f44
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-02 01:42:25 +0530

    shadows

--- App.js
+++ App.js
@@ -17,11 +17,13 @@ const App = () => {
             backgroundColor: 'dodgerblue',
             width: 100,
             height: 100,
-            borderWidth: 10,
-            borderColor: 'royalblue',
-            borderRadius: 50,
-            // borderTopWidth: 20,
-            // borderTopLeftRadius: 50,
+            // for IOS
+            // shadowColor: 'grey',
+            // shadowOffset: {width: 0, height: 10},
+            // shadowOpacity: 1,
+            // shadowRadius: 10,
+            // for Android
+            elevation: 20,
           }
         }
       >
