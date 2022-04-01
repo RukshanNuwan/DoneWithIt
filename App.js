f61fb8f179701744c0df5f8a794d27fd7a145544
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-01 21:33:59 +0530

    borders

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import {View} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <ViewImageScreen/>
+    <View
+      style={
+        {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }
+      }
+    >
+      <View
+        style={
+          {
+            backgroundColor: 'dodgerblue',
+            width: 100,
+            height: 100,
+            borderWidth: 10,
+            borderColor: 'royalblue',
+            borderRadius: 50,
+            // borderTopWidth: 20,
+            // borderTopLeftRadius: 50,
+          }
+        }
+      >
+      </View>
+    </View>
   );
-}
+};
+
+export default App;
