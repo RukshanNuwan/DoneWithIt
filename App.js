5a61f8dc243cecd72d87157f05256bbd7432482f
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-02 01:47:19 +0530

    padding & margin

--- App.js
+++ App.js
@@ -17,13 +17,31 @@ const App = () => {
             backgroundColor: 'dodgerblue',
             width: 100,
             height: 100,
-            // for IOS
-            // shadowColor: 'grey',
-            // shadowOffset: {width: 0, height: 10},
-            // shadowOpacity: 1,
-            // shadowRadius: 10,
-            // for Android
-            elevation: 20,
+            padding: 20,
+            paddingHorizontal: 10,
+            paddingLeft: 30,
+          }
+        }
+      >
+        <View
+          style={
+            {
+              backgroundColor: 'gold',
+              width: 50,
+              height: 50,
+            }
+          }
+        >
+        </View>
+      </View>
+
+      <View
+        style={
+          {
+            backgroundColor: 'tomato',
+            width: 100,
+            height: 100,
+            margin: 20,
           }
         }
       >
