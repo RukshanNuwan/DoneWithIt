980fdbcb230ab4d20118a560be10e38c72347a05
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-03 01:00:11 +0530

    flat list

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 
 const App = () => {
   return (
-    <ViewImageScreen/>
+    <MessagesScreen/>
   );
 };
 
