33bc928cf165893c3e75a5ce39cd1a99b9fb12d2
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-02 21:30:53 +0530

    improving the view image screen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,12 +1,17 @@
 import {Image, StyleSheet, View} from "react-native";
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 
 import colors from '../config/colors';
 
 const ViewImageScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name='close' color='#fff' size={30}/>
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons name='trash-can-outline' color='#fff' size={30}/>
+      </View>
       <Image source={require("../assets/chair.jpg")} style={styles.image}/>
     </View>
   );
@@ -18,17 +23,11 @@ const styles = StyleSheet.create({
     backgroundColor: colors.black,
   },
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
   },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
