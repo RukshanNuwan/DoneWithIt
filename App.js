1b4a97d40ba2ecec426518ac35e79530fdf0d979
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-04 14:12:18 +0530

    building the icon component

--- App.js
+++ App.js
@@ -5,11 +5,15 @@ import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 
 const App = () => {
   return (
-    <MessagesScreen/>
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red"/>
+    </Screen>
   );
 };
 
