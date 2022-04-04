31fe0df52c7a0ad6ebfdd830624bc95ae4427ed2
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-04 17:58:57 +0530

    extracting the default styles

--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,31 +1,26 @@
-import {Platform, StyleSheet, TextInput, View} from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
 import {MaterialCommunityIcons} from "@expo/vector-icons";
 
-import colors from "../config/colors";
+import defaultStyles from '../config/styles';
 
 const AppTextInput = ({icon, ...otherProps}) => {
   return (
     <View style={styles.container}>
-      {icon && <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon}/>}
-      <TextInput style={styles.textInput} {...otherProps}/>
+      {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon}/>}
+      <TextInput style={defaultStyles.text} {...otherProps}/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: 'row',
     alignItems: 'center',
     width: '100%',
     padding: 15,
     marginVertical: 10,
-  },
-  textInput: {
-    fontSize: 18,
-    fontFamily: Platform.OS === 'android' ? "Roboto" : "Avenir",
-    color: colors.dark,
   },
   icon: {
     marginRight: 10,
