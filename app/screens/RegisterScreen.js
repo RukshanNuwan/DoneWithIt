537214c15737833c2969fcd3945abcc42482cfad
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-15 01:56:51 +0530

    adding an overlay

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -41,48 +41,50 @@ const RegisterScreen = () => {
   };
 
   return (
-    <Screen style={styles.container}>
+    <>
       <ActivityIndicator visible={registerApi.loading || loginApi.loading}/>
 
-      <AppForm
-        initialValues={{name: "", email: "", password: ""}}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-      >
-        {error && (
-          <AppText style={styles.error}>{error}</AppText>
-        )}
+      <Screen style={styles.container}>
+        <AppForm
+          initialValues={{name: "", email: "", password: ""}}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+        >
+          {error && (
+            <AppText style={styles.error}>{error}</AppText>
+          )}
 
-        <AppFormField
-          autoCorrect={false}
-          icon="account"
-          name="name"
-          placeholder="Name"
-        />
+          <AppFormField
+            autoCorrect={false}
+            icon="account"
+            name="name"
+            placeholder="Name"
+          />
 
-        <AppFormField
-          autoCapitalize="none"
-          autoCorrect={false}
-          icon="email"
-          keyboardType="email-address"
-          name="email"
-          placeholder="Email"
-          textContentType="emailAddress"
-        />
+          <AppFormField
+            autoCapitalize="none"
+            autoCorrect={false}
+            icon="email"
+            keyboardType="email-address"
+            name="email"
+            placeholder="Email"
+            textContentType="emailAddress"
+          />
 
-        <AppFormField
-          autoCapitalize="none"
-          autoCorrect={false}
-          icon="lock"
-          name="password"
-          placeholder="Password"
-          secureTextEntry
-          textContentType="password"
-        />
+          <AppFormField
+            autoCapitalize="none"
+            autoCorrect={false}
+            icon="lock"
+            name="password"
+            placeholder="Password"
+            secureTextEntry
+            textContentType="password"
+          />
 
-        <SubmitButton title="Register"/>
-      </AppForm>
-    </Screen>
+          <SubmitButton title="Register"/>
+        </AppForm>
+      </Screen>
+    </>
   );
 };
 
