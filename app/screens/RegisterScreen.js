09fdc582693be388266734b5c6a7b972ce963a2b
RukshanNuwan <rukshannuwanbit@gmail.com>
2022-04-15 01:50:30 +0530

    showing an activity indicator

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -3,12 +3,14 @@ import {StyleSheet} from "react-native";
 import * as Yup from "yup";
 
 import useAuth from "../auth/useAuth";
+import useApi from "../hooks/useApi";
 import Screen from "../components/Screen";
 import {AppForm, AppFormField, SubmitButton} from "../components/forms";
 import usersApi from '../api/users';
 import authApi from '../api/auth';
 import AppText from "../components/AppText";
 import colors from "../config/colors";
+import ActivityIndicator from "../components/ActivityIndicator";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -19,9 +21,11 @@ const validationSchema = Yup.object().shape({
 const RegisterScreen = () => {
   const [error, setError] = useState();
   const auth = useAuth();
+  const registerApi = useApi(usersApi.register);
+  const loginApi = useApi(authApi.login);
 
   const handleSubmit = async (userInfo) => {
-    const result = await usersApi.register(userInfo);
+    const result = await registerApi.request(userInfo);
 
     if (!result.ok) {
       if (result.data) {
@@ -32,12 +36,14 @@ const RegisterScreen = () => {
       }
     }
 
-    const {data: authToken} = await authApi.login(userInfo.email, userInfo.password);
+    const {data: authToken} = await loginApi.request(userInfo.email, userInfo.password);
     await auth.logIn(authToken);
   };
 
   return (
     <Screen style={styles.container}>
+      <ActivityIndicator visible={registerApi.loading || loginApi.loading}/>
+
       <AppForm
         initialValues={{name: "", email: "", password: ""}}
         onSubmit={handleSubmit}
@@ -86,6 +92,7 @@ const styles = StyleSheet.create({
   },
   error: {
     color: colors.danger,
+    marginVertical: 10,
   },
 });
 
